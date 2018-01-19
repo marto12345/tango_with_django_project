1dd252dcc0ab28c74008a5076d150af1ce7513ce
Martin Dimitrov <2247164D@student.gla.ac.uk>
2018-01-19 21:54:39 +0000

    trial from own computer

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
@@ -22,4 +24,4 @@ urlpatterns = [
     url(r'^rango/',include('rango.urls')),
     url(r'^admin/',admin.site.urls),
     url(r'^$',views.about,name='about'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
