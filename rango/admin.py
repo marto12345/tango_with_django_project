cea188678e800fd187271102a77fb41f5de15663
Martin Dimitrov <2247164D@student.gla.ac.uk>
2018-01-25 08:21:39 +0000

    begin chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -4,3 +4,8 @@ from rango.models import Category, Page
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from rango.models import Category
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request,'rango/index.html',context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories' : category_list}
+    #context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request,'rango/index.html',context_dict)
     #return HttpResponse('Rango says hey there partner!<br/><a href="/rango/about/">About</a>')
 def about(request):
     context_dict = {}
